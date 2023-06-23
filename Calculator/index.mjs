a06eaf8d5a88b610a052e2879729a141f5aadda1
Ahmed Hamza <ahmedarif2882@gmail.com>
2023-06-23 09:50:40 +0500

    bug fixes

---
+++ Calculator/index.mjs
@@ -0,0 +1,24 @@
+let calculate = ()=> {
+    let num1 = +document.querySelector("#num1").value
+    let num2 = +document.querySelector("#num2").value
+    let resultEl = document.querySelector("#result")
+    let operator = document.getElementById("operator").value;
+
+    switch (operator) {
+        case "+":
+        result = num1 + num2;
+        break;
+        case "-":
+        result = num1 - num2;
+        break;
+        case "*":
+        result = num1 * num2;
+        break;
+        case "/":
+        result = num1 / num2;
+        break;
+        default:
+        result = "Invalid operator";
+    }
+    document.getElementById("result").innerHTML = `The result of ${num1} ${operator} ${num2} is ${result}.`;
+}
