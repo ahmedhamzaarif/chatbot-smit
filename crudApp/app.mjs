1a8161defcbaaf41e6d9ca419a2a28fbf6712ba9
Ahmed Hamza <ahmedarif2882@gmail.com>
2023-07-24 11:49:13 +0500

    cors url added

--- crudApp/app.mjs
+++ crudApp/app.mjs
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000
 
 
 var corsOptions = {
-  origin: 'http://localhost:3000/',
+  origin: 'http://localhost:3000/', //frontend link
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
