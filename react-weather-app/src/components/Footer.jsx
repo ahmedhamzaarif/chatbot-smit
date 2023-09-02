7e24e492d3122bec3be4e80c27a2b1d738b9cc05
Ahmed Hamza <ahmedarif2882@gmail.com>
2023-09-02 10:45:31 +0500

    Portfolio Link Added in Footer

--- react-weather-app/src/components/Footer.jsx
+++ react-weather-app/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const year = new Date().getFullYear() 
   return (
     <div className='bg-slate-900 text-center p-3 fixed bottom-0 text-slate-400 text-sm w-full'>
-        &copy; {year} Weather Canvas —  <a className='text-slate-100 font-semibold'>@AhmedHamzaArif</a> 
+        &copy; {year} Weather Canvas —  <a href="https://ahmedhamza.pk/" className='text-slate-100 font-semibold'>@AhmedHamzaArif</a> 
     </div>
   )
 }
