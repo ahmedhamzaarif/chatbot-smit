d4a213a465c7334051967c25eff0a0b6b47f1b4e
Ahmed Hamza <ahmedarif2882@gmail.com>
2023-09-12 10:53:55 +0500

    post & get done

--- crud-pinecone/app.mjs
+++ crud-pinecone/app.mjs
@@ -6,51 +6,29 @@ import morgan from 'morgan';
 import cors from 'cors'
 import path from 'path';
 const __dirname = path.resolve();
-// import { PineconeClient } from "@pinecone-database/pinecone";
 import { Pinecone } from '@pinecone-database/pinecone';
 import OpenAI from "openai";
-// import { config } from 'dotenv';
-// config()
 import "dotenv/config.js";
+import './config/index.mjs'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
-import './config/index.mjs'
-
-// const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.9ha3mra.mongodb.net/?retryWrites=true&w=majority`
-// const client = new MongoClient(mongodbURI);
-// const database = client.db('ecom');
-// const productsCollection = database.collection('products');
-
-// const pinecone = new PineconeClient();
-// await pinecone.init({
-//   environment: process.env.PINECONE_ENVIRONMENT,
-//   apiKey: process.env.PINECONE_API_KEY,
-// });
 
 const pinecone = new Pinecone({ 
     apiKey: process.env.PINECONE_API_KEY,
     environment: process.env.PINECONE_ENVIRONMENT
 })
-await pinecone.listIndexes()
+// await pinecone.listIndexes()
 
 
 const app = express();
 app.use(express.json());
-// app.use(cors(["http://localhost:3000", "127.0.0.1"]));
-// app.use(cors(["http://localhost:3000/"]));
-// app.use(cors())
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}))
+app.use(cors(["http://localhost:3000", "127.0.0.1"]));
 app.use(morgan('combined'));
 
-
 // Get All
-app.get('/api/v1/story/', async (req, res) => {
+app.get('/api/v1/stories/', async (req, res) => {
   const queryText = ""
   // const queryText = "retreated"
   const response = await openai.embeddings.create({
@@ -62,113 +40,124 @@ app.get('/api/v1/story/', async (req, res) => {
   console.log("vector: ", vector);
 
   const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
-  const queryResponse = await index.query({
-    queryRequest: {
+  await index.query({
       vector: vector,
       // id: "vec1",
       topK: 100,
-      includeValues: true,
+      includeValues: false,
       includeMetadata: true,
       // namespace: process.env.PINECONE_NAME_SPACE
-    }
-  });
-
-  queryResponse.matches.map(eachMatch => {
-    console.log(`score ${eachMatch.score.toFixed(1)} => ${JSON.stringify(eachMatch.metadata)}\n\n`);
+  }).then((resp)=>{
+    res.send({
+      response: resp,
+      message:"All Stories Are Here"
+    })
+  }).catch((error)=>{
+    res.status(400).send({
+      message: "Error While Getting Stories",
+      error
+    })
   })
-  console.log(`${queryResponse.matches.length} records found `);
-
-  res.send(queryResponse.matches)
-
-})
+});
 
 // Upload 
 app.post('/api/v1/story', async (req, res) => {
   console.log("req.body: ", req.body);
+  try{
+    const response = await openai.embeddings.create({
+      model: "text-embedding-ada-002",
+      input: `${req.body?.title} ${req.body?.text}`,
+    });
+    console.log("response?.data: ", response?.data);
+    const vector = response?.data[0]?.embedding
+    console.log("vector: ", vector);
+    const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+    const upsertResponse = await index.upsert([{
+      id: nanoid(),
+      values: vector,
+      metadata: {
+        title: req.body.title,
+        text: req.body.text
+      },
+    }])
+
+    console.log(upsertResponse)
+
+
+    res.send({
+      upsertResponse,
+      message: "story created successfully"
+    });
+
+  } catch(error){
+    res.status(500).send({
+      error,
+      message: "Failed to Create Story Plz Try Again"
+    })
+  }
+});
+
+// update 
+app.put("/api/v1/story/:id", async (req, res) => {
+
   const response = await openai.embeddings.create({
     model: "text-embedding-ada-002",
-    input: `${req.body?.title} ${req.body?.body}`,
+    input: `${req.body?.title} ${req.body?.text}`,
   });
   console.log("response?.data: ", response?.data);
-  const vector = response?.data[0]?.embedding
-  console.log("vector: ", vector);
-  const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
-  const upsertRequest = {
-    vectors: [
-      {
-        id: nanoid(),
-        values: vector,
-        metadata: {
-          title: req.body?.title,
-          body: req.body?.body,
-        }
-      }
-    ],
-    // namespace: process.env.PINECONE_NAME_SPACE,
-  };
+  const vector = response.data[0].embedding
+
+  const index = pinecone.index(process.env.PINECONE_INDEX_NAME);
   try {
-    const upsertResponse = await index.upsert({ upsertRequest });
+    const upsertResponse = await index.upsert([{
+      id: nanoid(),
+      values: vector,
+      metadata: {
+        title: req.body.title,
+        text: req.body.text
+      },
+    }])
     console.log("upsertResponse: ", upsertResponse);
 
     res.send({
-      message: "story created successfully"
+      message: "story updated successfully"
     });
   } catch (e) {
     console.log("error: ", e)
     res.status(500).send({
-      message: "failed to create story, please try later"
+      message: "failed to update story, please try later"
     });
   }
-})
-
-
-// update 
-app.put("/product/:id", async (req, res) => {
-
-  if (
-    !req.body.name
-    && !req.body.price
-    && !req.body.description) {
-
-    res.status(403).send(`
-      required parameter missing. 
-      atleast one parameter is required: name, price or description to complete update
-      example JSON request body:
-      {
-        name: "abc product",
-        price: "$23.12",
-        description: "abc product description"
-      }`);
-  }
-
- await productsCollection.findOneAndUpdate( {_id:new ObjectId(req.params.id)},{
-  $set:{
-    name: req.body.name,
-    price: req.body.price,
-    description: req.body.description
-  }
- }).then(()=>{
-  res.send("Product Updated!")
- }).catch((err)=>{
-  res.send(err)
- })
 });
 
+// delete
+app.delete("/api/v1/story/:id", async (req, res) => {
+  try {
 
-// delete product
-app.delete('/product/:id', async (req, res) => {
-  await productsCollection.findOneAndDelete({
-    _id: new ObjectId(req.params.id)
-  }).then(()=>{
-    res.send("Product Deleted!")
-  }).catch((err)=>{
-    res.send(err)
-  })
-})
+    const index = pinecone.index(process.env.PINECONE_INDEX_NAME);
+
+    const deleteResponse = await index.deleteOne(String(req.params.id))
+
+    res.send({
+      deleteResponse,
+      message: "Deleted Successfully"
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      message: "Failed to delete Story"
+    })
+  }
+
+});
 
 
 app.get(express.static(path.join(__dirname, "./web/build")))
 app.use("/", express.static(path.join(__dirname, './web/build')))
+
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+})
 
 const port = process.env.PORT || 5001
 app.listen(port, () => {
--- crud-pinecone/web/src/App.jsx
+++ crud-pinecone/web/src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react';
-import './App.css';
 import axios from 'axios'
 
 const baseUrl = "http://localhost:5001"
@@ -9,7 +8,7 @@ function App() {
   const bodyInputRef = useRef(null)
   const [isLoading, setIsLoading] = useState(false)
   const [alert, setAlert] = useState("")
-  const [data, setdata] = useState([])
+  const [data, setData] = useState([])
   
   useEffect(() => {
     if(alert){
@@ -30,7 +29,7 @@ function App() {
       setIsLoading(true);
       const resp = await axios.get(`${baseUrl}/api/v1/stories`)
       console.log(resp.data);
-      setData(resp.data);
+      setData(resp.data?.response?.matches);
 
       setIsLoading(false);
     } catch (e) {
@@ -49,7 +48,7 @@ function App() {
       // const response = await axios.post(`${baseUrl}/api/v1/story`)
       const response = await axios.post(`${baseUrl}/api/v1/story`, {
         title: titleInputRef.current.value,
-        body: bodyInputRef.current.value,
+        text: bodyInputRef.current.value,
       });
 
       console.log('response: ', response.data)
@@ -64,24 +63,30 @@ function App() {
 
   }
   return (
-    <>
-      <h1>Social Stories</h1>
-      <form onSubmit={postStory}>
-        <label htmlFor="titleInput">Title: </label>
-        <input type="text" id="bodyInput" maxLength={20} minLength={2} required ref={titleInputRef}/>
+    <div className='p-10'>
+      <h1 className='text-center text-5xl mb-5 font-bold'>Social Stories</h1>
 
-        <br />
-
-        <label htmlFor="bodyInput">What's on your mind: </label>
-        <textarea type="text" id="bodyInput" cols="30" rows="10" maxLength={999} minLength={10} required ref={bodyInputRef}></textarea>
-        
-        <br />
-
-        <button type="submit">Post</button>
+      <form onSubmit={postStory} className='md:w-1/2 mx-auto mb-10'>
+        <label htmlFor="titleInput" className='w-full'>Title: </label>
+        <input type="text" id="bodyInput" maxLength={20} minLength={2} required ref={titleInputRef} className='w-full border-2 mb-5 rounded-md p-2'/>
+        <label htmlFor="bodyInput" className='w-full'>What's on your mind: </label>
+        <textarea type="text" id="bodyInput" rows="5" maxLength={999} minLength={10} required ref={bodyInputRef} className='p-2 border border-2 rounded-md w-full mb-5'></textarea>
+        <button type="submit" className='bg-slate-500 rounded-md px-8 py-2 text-white'>Post</button>
       </form>
+
       {alert && <div className='alert'>{alert}</div>}
-      {isLoading ? <div>Loading...</div>: ''}
-    </>
+      {isLoading ? <div className='text-white w-fit bg-slate-500 p-2 rounded-md'>Loading...</div>: ''}
+
+    
+      <div className="grid grid-cols-3 gap-4">
+        {data.map((item, i)=>(
+          <div className='shadow-xl p-5 bg-slate-50' key={i}>
+          <h2>{item?.metadata?.title}</h2>
+          <p>{item?.metadata?.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
